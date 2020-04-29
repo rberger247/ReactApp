1bddfbe442c3f25f3714dffcb0e8350ce1fb3377
rafael berger <Rafiberger613@gmail.com>
2020-04-29 10:58:35 -0400

    change app

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,10 +1,9 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-module.exports = function(app) {
+const { createProxyMiddleware } = require("http-proxy-middleware");
+module.exports = function (app) {
   app.use(
-    '/api',
+    ["/api", "/auth/google"],
     createProxyMiddleware({
-      target: 'http://localhost:5000',
-      changeOrigin: true
+      target: "http://localhost:5000",
     })
   );
 };
